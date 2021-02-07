f21b72183f6a8fb049587875231b4470b982daf5
Sneha20011997 <77962315+Sneha20011997@users.noreply.github.com>
2021-02-07 12:43:58 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,7 @@
+function onclickExam(){
+     
+     var name = document.getElementById("name1").value;
+     var password=document.getElementById("password1").value;
+     alert("Output is:"+name+password);
+     
+}
